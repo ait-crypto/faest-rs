2d639ba78d7f27b38358595d50b84137b79b33be
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2024-04-15 08:49:41 +0200

    Remove target/ from git

--- faest/target/debug/deps/faest-1ea90742bfe12569.d
+++
@@ -1,6 +0,0 @@
-/home/clementnaves/Desktop/Code/faest-rs/faest/target/debug/deps/libfaest-1ea90742bfe12569.rmeta: src/main.rs src/fields.rs
-
-/home/clementnaves/Desktop/Code/faest-rs/faest/target/debug/deps/faest-1ea90742bfe12569.d: src/main.rs src/fields.rs
-
-src/main.rs:
-src/fields.rs:
--- faest/target/debug/deps/faest-3403c33b86cc5aa6.d
+++
@@ -1,7 +0,0 @@
-/home/clementnaves/Desktop/Code/faest-rs/faest/target/debug/deps/libfaest-3403c33b86cc5aa6.rmeta: src/lib.rs src/fields.rs src/fields_test.rs
-
-/home/clementnaves/Desktop/Code/faest-rs/faest/target/debug/deps/faest-3403c33b86cc5aa6.d: src/lib.rs src/fields.rs src/fields_test.rs
-
-src/lib.rs:
-src/fields.rs:
-src/fields_test.rs:
