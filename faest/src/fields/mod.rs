0e50906e2c6cc7cbf2f892061db6936e5d3227a9
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2024-08-22 17:21:10 +0200

    Add marker trait for fields

--- faest/src/fields/mod.rs
+++ faest/src/fields/mod.rs
@@ -1,5 +1,20 @@
 pub(crate) mod large_fields;
 pub(crate) mod small_fields;
 
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
+
 pub use large_fields::{BigGaloisField, GF128, GF192, GF256};
 pub use small_fields::{GaloisField, GF64, GF8};
+
+/// Trait covering the basic functionality of a field
+pub trait Field:
+    Sized
+    + Default
+    + Add<Self, Output = Self>
+    + AddAssign
+    + Sub<Self, Output = Self>
+    + SubAssign
+    + Mul<Self, Output = Self>
+    + MulAssign
+{
+}
