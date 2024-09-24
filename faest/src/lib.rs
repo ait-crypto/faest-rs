07a74d70c352e515d88219bbe7adb1769548a0f2
Clement Naves <clement.naves@ait.ac.at>
2024-09-24 13:26:41 +0200

    Adding lib modifications

--- faest/src/lib.rs
+++ faest/src/lib.rs
@@ -1,16 +1,13 @@
+mod aes;
+mod em;
 mod fields;
 mod random_oracles;
+mod rijndael_32;
 mod universal_hashing;
 mod vc;
 mod vole;
 
-mod aes;
-mod em;
-mod rijndael_32;
-
 pub mod faest;
-#[cfg(test)]
-mod faest_test;
 
 pub mod parameter;
 
