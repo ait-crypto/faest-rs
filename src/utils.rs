5450b7f1c26e23eac75a2fc80d5b927521fcc444
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2024-11-07 16:28:31 +0100

    Fix clippy warning

--- src/utils.rs
+++ src/utils.rs
@@ -42,26 +42,23 @@ where
     O: OWFParameters,
     Tau: TauParameters,
 {
-    for index in (0..Tau::Tau::USIZE)
-        .map(|i| {
-            Tau::decode_challenge_as_iter(chall3, i)
-                .enumerate()
-                .filter_map(move |(j, delta_j)| {
-                    if delta_j != 0 {
-                        Some(if i < Tau::Tau0::USIZE {
-                            Tau::K0::USIZE * i + j
-                        } else {
-                            Tau::Tau0::USIZE * Tau::K0::USIZE
-                                + Tau::K1::USIZE * (i - Tau::Tau0::USIZE)
-                                + j
-                        })
+    for index in (0..Tau::Tau::USIZE).flat_map(|i| {
+        Tau::decode_challenge_as_iter(chall3, i)
+            .enumerate()
+            .filter_map(move |(j, delta_j)| {
+                if delta_j != 0 {
+                    Some(if i < Tau::Tau0::USIZE {
+                        Tau::K0::USIZE * i + j
                     } else {
-                        None
-                    }
-                })
-        })
-        .flatten()
-    {
+                        Tau::Tau0::USIZE * Tau::K0::USIZE
+                            + Tau::K1::USIZE * (i - Tau::Tau0::USIZE)
+                            + j
+                    })
+                } else {
+                    None
+                }
+            })
+    }) {
         for (gq_k, d_k) in zip(gq[index].iter_mut(), d).take(O::LBYTES::USIZE) {
             *gq_k ^= d_k;
         }
