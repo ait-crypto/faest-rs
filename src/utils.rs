c68a29b504e14e13fc6da25e54e83be90c51ce7e
alecolo129 <alessandro.colombo129@gmail.com>
2025-03-04 11:34:36 +0100

    Refactor

--- src/utils.rs
+++ src/utils.rs
@@ -26,11 +26,19 @@ pub(crate) trait Reader {
     }
 }
 
+/// Directly convert `chal[start_bit...start_bit+k]` into a 16-bit integer
 fn chall_to_u16(chall: &[u8], start_bit: usize, mut k: usize) -> u16 {
+
+    // As by current specification, we assume k<16 
+    debug_assert!(k<16);
+    debug_assert!(chall.len() >= k);
+
+
     let mut result = 0u16;
 
-    let byte_idx = start_bit / 8;
-    let bit_off = start_bit % 8;
+    // Starting byte and offset within byte
+    let byte_idx = start_bit / 8; 
+    let bit_off = start_bit % 8;  
 
     // Take bits from lo to end of first byte
     let taken = std::cmp::min(k, 8 - bit_off);
@@ -38,16 +46,17 @@ fn chall_to_u16(chall: &[u8], start_bit: usize, mut k: usize) -> u16 {
     result |= (chall[byte_idx] as u16 >> bit_off) & mask;
     k -= taken;
 
-    // Take bits from next byte
+    // If needed bits from next byte_idx + 1
     if k != 0 {
         let taken = std::cmp::min(k, 8);
         let mask = (1 << taken) - 1;
         result |= (chall[byte_idx + 1] as u16 & mask) << 8 - bit_off;
         k -= taken;
     }
-
+    
+    // If needed, take bits from byte_idx + 2
     if k != 0 {
-        let mask = (1 << k) - 1;
+        let mask = (1 << k) - 1; // Assuming k<16
         result |= (chall[byte_idx + 2] as u16 & mask) << 16 - bit_off;
     }
 
@@ -55,15 +64,18 @@ fn chall_to_u16(chall: &[u8], start_bit: usize, mut k: usize) -> u16 {
 }
 
 pub(crate) fn decode_all_chall_3<TAU: TauParameters>(chall: &[u8]) -> GenericArray<u16, TAU::Tau> {
+    
     let k = TAU::K::USIZE;
 
-    (0..TAU::Tau1::USIZE)
-        .map(|i| chall_to_u16(chall, TAU::tau1_offset_unchecked(i), k))
-        .chain(
-            (TAU::Tau1::USIZE..TAU::Tau::USIZE)
-                .map(|i| chall_to_u16(chall, TAU::tau0_offset_unchecked(i), k - 1)),
-        )
-        .collect()
+    // Compute Delta_i[0...Tau1)
+    let first_half = (0..TAU::Tau1::USIZE)
+        .map(|i| chall_to_u16(chall, TAU::tau1_offset_unchecked(i), k));
+    
+    // Compute Delta_i[Tau1..Tau)
+    let second_half = (TAU::Tau1::USIZE..TAU::Tau::USIZE)
+        .map(|i| chall_to_u16(chall, TAU::tau0_offset_unchecked(i), k - 1));
+    
+    first_half.chain(second_half).collect()
 }
 
 // pub(crate) type Field<O> = <<O as OWFParameters>::BaseParams as BaseParameters>::Field;
@@ -135,10 +147,7 @@ pub(crate) fn decode_all_chall_3<TAU: TauParameters>(chall: &[u8]) -> GenericArr
 pub(crate) mod test {
     use std::{fs::File, path::Path};
 
-    use generic_array::GenericArray;
     use serde::de::DeserializeOwned;
-
-    use crate::parameter::{Tau128Fast, Tau128Small};
 
     pub(crate) fn read_test_data<T: DeserializeOwned>(path: &str) -> Vec<T> {
         File::open(
@@ -156,14 +165,4 @@ pub(crate) mod test {
         .unwrap_or_else(|_| panic!("Failed to read JSON test data from {}", path))
     }
 
-    #[test]
-    pub fn test_chall() {
-        let chal = GenericArray::from_array([
-            0x71, 0x55, 0xb8, 0xf0, 0xde, 0x65, 0xbe, 0xd1, 0x93, 0xb8, 0x61, 0x5b, 0xcd, 0xe6,
-            0x89, 0x00,
-        ]);
-
-        let i_delta = super::decode_all_chall_3::<Tau128Fast>(chal.as_slice());
-        println!("{:?}", i_delta);
-    }
 }
