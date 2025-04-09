e5e007eef8bd2817a8fd56653f7fac2af1a40012
Alessandro Colombo <alessandro.colombo129@gmail.com>
2025-04-09 14:12:14 +0200

    Implement key expansion

--- src/verifier/key_expansion.rs
+++ src/verifier/key_expansion.rs
@@ -1,139 +1,180 @@
+use super::vole_commitments::{VoleCommits, VoleCommitsRef};
+use crate::{
+    aes::{
+        AddRoundKey, AddRoundKeyAssign, AddRoundKeyBytes, BytewiseMixColumns, InverseAffine,
+        InverseShiftRows, MixColumns, SBoxAffine, ShiftRows, StateBitsCommits,
+        StateBitsSquaredCommits, StateBytesCommits, StateBytesSquaredCommits, StateToBytes,
+    },
+    fields::{
+        large_fields::{Betas, ByteCombineSquared, SquareBytes},
+        ByteCombine, ByteCommitment, ByteCommits, ByteCommitsRef, Sigmas, Square,
+    },
+    parameter::{OWFField, OWFParameters},
+    rijndael_32::RCON_TABLE,
+    universal_hashing::ZKVerifyHasher,
+    utils::get_bit,
+};
+use generic_array::{
+    typenum::{Prod, Quot, Unsigned, U2, U4, U8},
+    ArrayLength, GenericArray,
+};
+use itertools::iproduct;
+use std::ops::{AddAssign, Deref, Index};
+use std::{convert::AsRef, process::Output};
 
+pub(crate) fn key_exp_fwd<O>(
+    w: VoleCommitsRef<OWFField<O>, O::LKE>,
+) -> VoleCommits<OWFField<O>, O::PRODRUN128>
+where
+    O: OWFParameters,
+{
+    // ::1
+    let mut y_tags = GenericArray::default_boxed();
+    y_tags[..O::LAMBDA::USIZE].copy_from_slice(&w.scalars[..O::LAMBDA::USIZE]);
 
-    pub(crate) fn key_exp_fwd<O>(
-        w: ByteCommitsRef<OWFField<O>, O::LKEBytes>,
-    ) -> ByteCommits<OWFField<O>, O::PRODRUN128Bytes>
-    where
-        O: OWFParameters,
-    {
-        // ::1
-        let mut y = GenericArray::default_boxed();
-        let mut y_tags = GenericArray::default_boxed();
-        y[..O::LAMBDABYTES::USIZE].copy_from_slice(&w.keys[..O::LAMBDABYTES::USIZE]);
-        y_tags[..O::LAMBDA::USIZE].copy_from_slice(&w.tags[..O::LAMBDA::USIZE]);
+    // ::2
+    let mut i_wd = O::LAMBDA::USIZE;
 
-        // ::2
-        let mut i_wd = O::LAMBDA::USIZE;
+    for j in O::NK::USIZE..(4 * (O::R::USIZE + 1)) {
+        // ::5
+        if (j % O::NK::USIZE == 0) || ((O::NK::USIZE > 6) && (j % O::NK::USIZE == 4)) {
+            // ::6
+            y_tags[32 * j..32 * j + 32].copy_from_slice(&w.scalars[i_wd..i_wd + 32]);
 
-        for j in O::NK::USIZE..(4 * (O::R::USIZE + 1)) {
-            // ::5
-            if (j % O::NK::USIZE == 0) || ((O::NK::USIZE > 6) && (j % O::NK::USIZE == 4)) {
-                // ::6
-                y[4 * j..4 * j + 4].copy_from_slice(&w.keys[i_wd / 8..i_wd / 8 + 4]);
-                y_tags[32 * j..32 * j + 32].copy_from_slice(&w.tags[i_wd..i_wd + 32]);
+            // ::7
+            i_wd += 32;
+        } else {
+            // ::9-10
+            for i in 0..32 {
+                y_tags[32 * j + i] = y_tags[32 * (j - O::NK::USIZE) + i] + y_tags[32 * (j - 1) + i];
+            }
+        }
+    }
 
-                // ::7
-                i_wd += 32;
-            } else {
-                // ::9-10
-                for i in 0..4 {
-                    y[4 * j + i] = y[4 * (j - O::NK::USIZE) + i] ^ y[4 * (j - 1) + i];
+    VoleCommits {
+        scalars: y_tags,
+        delta: w.delta,
+    }
+}
 
-                    for i_0 in 8 * i..8 * i + 8 {
-                        y_tags[32 * j + i_0] =
-                            y_tags[32 * (j - O::NK::USIZE) + i_0] + y_tags[32 * (j - 1) + i_0];
+pub(crate) fn key_exp_bkwd<'a, O>(
+    x: VoleCommitsRef<'a, OWFField<O>, O::DIFFLKELAMBDA>,
+    xk: VoleCommitsRef<'a, OWFField<O>, O::PRODRUN128>,
+) -> VoleCommits<'a, OWFField<O>, Prod<O::SKE, U8>>
+where
+    O: OWFParameters,
+{
+    let mut y_tag = GenericArray::default_boxed();
+
+    let mut iwd = 0;
+
+    let rcon_evry = 4 * (O::LAMBDA::USIZE / 128);
+
+    for j in 0..O::SKE::USIZE {
+        // ::7
+
+        let xt_tag: GenericArray<OWFField<O>, U8> = (0..8)
+            .map(|i| {
+                let mut x_tilde_i = x.scalars[8 * j + i] + xk.scalars[iwd + 8 * (j % 4) + i];
+
+                // ::8
+                if j % rcon_evry == 0 {
+                    if get_bit(&[RCON_TABLE[j / rcon_evry]], i) != 0 {
+                        x_tilde_i += x.delta;
                     }
                 }
+
+                x_tilde_i
+            })
+            .collect();
+
+        inverse_affine_byte::<O>(&mut y_tag[8 * j..8 * j + 8], &xt_tag, x.delta);
+
+        // ::12
+        if j % 4 == 3 {
+            if O::LAMBDA::USIZE != 256 {
+                iwd += O::LAMBDA::USIZE;
+            } else {
+                iwd += 128;
             }
         }
-
-        ByteCommits::new(y, y_tags)
     }
 
-    pub(crate) fn key_exp_bkwd<O>(
-        x: ByteCommitsRef<OWFField<O>, O::DIFFLKELAMBDABytes>,
-        xk: ByteCommitsRef<OWFField<O>, O::PRODRUN128Bytes>,
-    ) -> ByteCommits<OWFField<O>, O::SKE>
-    where
-        O: OWFParameters,
-    {
-        let mut y = GenericArray::default_boxed();
-        let mut y_tag = GenericArray::default_boxed();
+    VoleCommits {
+        scalars: y_tag,
+        delta: x.delta,
+    }
+}
 
-        let mut iwd = 0;
-
-        let rcon_evry = 4 * (O::LAMBDA::USIZE / 128);
-
-        for j in 0..O::SKE::USIZE {
-            // ::7
-            let mut x_tilde = x.keys[j] ^ xk.keys[iwd / 8 + (j % 4)];
-
-            let xt_0: GenericArray<OWFField<O>, U8> = (0..8)
-                .map(|i| x.tags[8 * j + i] + xk.tags[iwd + 8 * (j % 4) + i])
-                .collect();
-
-            // ::8
-            if j % rcon_evry == 0 {
-                x_tilde ^= RCON_TABLE[j / rcon_evry];
-            }
-
-            inverse_affine_byte::<O>(x_tilde, &xt_0, &mut y[j], &mut y_tag[8 * j..8 * j + 8]);
-
-            // ::12
-            if j % 4 == 3 {
-                if O::LAMBDA::USIZE != 256 {
-                    iwd += O::LAMBDA::USIZE;
-                } else {
-                    iwd += 128;
-                }
-            }
-        }
-
-        ByteCommits::new(y, y_tag)
+pub fn inverse_affine_byte<O>(
+    y_tag: &mut [OWFField<O>],
+    x_tag: &GenericArray<OWFField<O>, U8>,
+    delta: &OWFField<O>,
+) where
+    O: OWFParameters,
+{
+    for i in 0..8 {
+        y_tag[i] = x_tag[(i + 8 - 1) % 8] + x_tag[(i + 8 - 3) % 8] + x_tag[(i + 8 - 6) % 8];
     }
 
-    pub(crate) fn key_exp_cstrnts<O>(
-        zk_hasher: &mut ZKProofHasher<OWFField<O>>,
-        w: ByteCommitsRef<OWFField<O>, O::LKEBytes>,
-    ) -> ByteCommits<OWFField<O>, O::PRODRUN128Bytes>
-    where
-        O: OWFParameters,
-        OWFField<O>: BigGaloisField + ByteCombine,
-        <<O as OWFParameters>::BaseParams as BaseParameters>::Field: PartialEq,
-    {
-        // ::1
-        let k = key_exp_fwd::<O>(w);
+    y_tag[0] += delta;
+    y_tag[2] += delta;
+}
 
-        // ::2
-        let w_flat = key_exp_bkwd::<O>(
-            w.get_commits_ref::<O::DIFFLKELAMBDABytes>(O::LAMBDABYTES::USIZE),
-            k.get_ref(),
+pub(crate) fn key_exp_cstrnts<'a, O>(
+    zk_hasher: &mut ZKVerifyHasher<OWFField<O>>,
+    w: VoleCommitsRef<'a, OWFField<O>, O::LKE>,
+) -> VoleCommits<'a, OWFField<O>, O::PRODRUN128>
+where
+    O: OWFParameters,
+{
+    // ::1
+    let k = key_exp_fwd::<O>(w);
+
+    // ::2
+    let w_flat = key_exp_bkwd::<O>(
+        w.get_commits_ref::<O::DIFFLKELAMBDA>(O::LAMBDA::USIZE),
+        k.get_ref(),
+    );
+
+    let mut iwd = 32 * (O::NK::USIZE - 1);
+
+    let mut do_rot_word = true;
+
+    // ::7
+    iproduct!(0..O::SKE::USIZE / 4, 0..4).for_each(|(j, r)| {
+        // ::11
+        let r_prime_inv = if do_rot_word { (4 + r - 3) % 4 } else { r };
+
+        // ::12-13
+        let k_hat =
+            OWFField::<O>::byte_combine_slice(&k[iwd + 8 * r_prime_inv..iwd + 8 * r_prime_inv + 8]);
+        let k_hat_sq = OWFField::<O>::byte_combine_sq_slice(
+            &k[iwd + 8 * r_prime_inv..iwd + 8 * r_prime_inv + 8],
         );
 
-        let mut iwd = 32 * (O::NK::USIZE - 1);
+        // ::14-15
+        let w_hat = OWFField::<O>::byte_combine_slice(&w_flat[32 * j + 8 * r..32 * j + 8 * r + 8]);
+        let w_hat_sq =
+            OWFField::<O>::byte_combine_sq_slice(&w_flat[32 * j + 8 * r..32 * j + 8 * r + 8]);
 
-        let mut do_rot_word = true;
+        // :: 19-20 (directly update zk_hahser with constraints)
+        zk_hasher.lift_and_process(&k_hat, &k_hat_sq, &w_hat, &w_hat_sq);
 
-        // ::7
-        iproduct!(0..O::SKE::USIZE / 4, 0..4).for_each(|(j, r)| {
-            // ::11
-            let r_prime_inv = if do_rot_word { (4 + r - 3) % 4 } else { r };
-
-            // ::12-13
-            let k_hat = k.get_field_commit(iwd / 8 + r_prime_inv);
-            let k_hat_sq = k.get_field_commit_sq(iwd / 8 + r_prime_inv);
-
-            // ::14-15
-            let w_hat = w_flat.get_field_commit(4 * j + r);
-            let w_hat_sq = w_flat.get_field_commit_sq(4 * j + r);
-
-            // :: 19-20 (directly update zk_hahser with constraints)
-            zk_hasher.lift_and_process(&k_hat, &k_hat_sq, &w_hat, &w_hat_sq);
-
-            if r == 3 {
-                // ::16
-                if O::LAMBDA::USIZE == 256 {
-                    do_rot_word = !do_rot_word;
-                }
-
-                // ::21
-                if O::LAMBDA::USIZE == 192 {
-                    iwd += 192;
-                } else {
-                    iwd += 128;
-                }
+        if r == 3 {
+            // ::16
+            if O::LAMBDA::USIZE == 256 {
+                do_rot_word = !do_rot_word;
             }
-        });
 
-        k
-    }
+            // ::21
+            if O::LAMBDA::USIZE == 192 {
+                iwd += 192;
+            } else {
+                iwd += 128;
+            }
+        }
+    });
+
+    k
+}
