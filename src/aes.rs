35d3fe7201de8f2ba5f97365c45a14b27f65be80
Alessandro Colombo <alessandro.colombo129@gmail.com>
2025-04-16 11:11:04 +0200

    Remove unnecessary function parameters

--- src/aes.rs
+++ src/aes.rs
@@ -1,37 +1,4 @@
-use aes::cipher::KeyInit;
-use generic_array::{
-    functional::FunctionalSequence,
-    typenum::{Prod, Unsigned, Zero, B1, U1, U10, U3, U32, U4, U8},
-    ArrayLength, GenericArray,
-};
-use itertools::multiunzip;
-use itertools::{iproduct, izip};
-use std::{
-    array, default,
-    mem::size_of,
-    ops::{Add, AddAssign, Mul, Sub},
-};
-
-use crate::{
-    fields::{
-        large_fields::{Betas, ByteCombineSquared, ByteCombineSquaredConstants, SquareBytes},
-        small_fields::{GF8, GF8_INV_NORM},
-        BigGaloisField, ByteCombine, ByteCombineConstants, Field, Sigmas, SumPoly,
-    },
-    internal_keys::PublicKey,
-    parameter::{BaseParameters, OWFField, OWFParameters, QSProof, TauParameters},
-    prover::{
-        byte_commitments::{ByteCommits, ByteCommitsRef},
-        field_commitment::{FieldCommitDegOne, FieldCommitDegThree, FieldCommitDegTwo},
-    },
-    rijndael_32::{
-        bitslice, convert_from_batchblocks, inv_bitslice, mix_columns_0, rijndael_add_round_key,
-        rijndael_key_schedule, rijndael_shift_rows_1, rijndael_sub_bytes, sub_bytes,
-        sub_bytes_nots, State, RCON_TABLE,
-    },
-    universal_hashing::{ZKHasher, ZKHasherInit, ZKHasherProcess, ZKProofHasher, ZKVerifyHasher},
-    utils::{contains_zeros, get_bits, xor_arrays},
-};
+use crate::parameter::OWFParameters;
 
 /// Trait for adding a round key to the state, generating a new state
 pub(crate) trait AddRoundKey<Rhs = Self> {
@@ -72,16 +39,16 @@ pub(crate) trait SBoxAffine<O: OWFParameters> {
 
 /// Trait for applying the AES shift rows transformation to the state in-place
 pub(crate) trait ShiftRows {
-    fn shift_rows<O: OWFParameters>(&mut self);
+    fn shift_rows(&mut self);
 }
 
 /// Trait for applying the AES S-box affine inverse transformation to the state in-place
 pub(crate) trait InverseAffine {
-    fn inverse_affine<O: OWFParameters>(&mut self);
+    fn inverse_affine(&mut self);
 }
 
 /// Trait for applying the AES mix columns transformation to the state in-place
-pub(crate) trait MixColumns<O: OWFParameters> {
+pub(crate) trait MixColumns<O> {
     fn mix_columns(&mut self, sq: bool);
 }
 
