74dcd25d35a872826af4a7f93a1358c6237ce7a6
Alessandro Colombo <alessandro.colombo@ait.ac.at>
2026-02-01 07:47:04 +0100

    Don't panic when valgrind unavailable #2

--- valgrind-bindings/src/lib.rs
+++ valgrind-bindings/src/lib.rs
@@ -4,6 +4,11 @@
 #![allow(unexpected_cfgs)]
 
 #[cfg(has_valgrind)]
+pub const HAS_VALGRIND: bool = true;
+#[cfg(not(has_valgrind))]
+pub const HAS_VALGRIND: bool = false;
+
+#[cfg(has_valgrind)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(not(has_valgrind))]
