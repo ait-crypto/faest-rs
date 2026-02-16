9c10ce4785cd577f3ae7ef0a978360dba28eef29
Alessandro Colombo <alessandro.colombo@ait.ac.at>
2026-02-16 20:58:29 +0100

    Add option for checking simd

--- faest-memcheck/test_with_valgrind.sh
+++ faest-memcheck/test_with_valgrind.sh
@@ -2,5 +2,35 @@
 
 set -e # Exit immediately if any command exits with a non-zero status
 
-cargo build --features valgrind
-valgrind --tool=memcheck ../target/debug/faest-memcheck
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+helpFunction()
+{
+  echo ""
+  echo "Usage: test_with_valgrind.sh [--opt-simd] -h"
+  echo -e "\t--opt-simd Tests optimized simd implementation (if supported by target architecture)"
+  echo -e "\t-h Print the script help message."
+  exit 1
+}
+
+for arg in "$@"; do
+   case "$arg" in
+      --opt-simd )
+        opt_simd=true ;;
+      h ) helpFunction ;;
+      ? ) helpFunction ;;
+      *)
+        helpFunction 
+        exit 1 ;;
+   esac
+done
+
+if [[ -v opt_simd ]] ; then
+    echo "Testing avx2 implementation..."
+    RUSTFLAGS="-C target-cpu=native" cargo build --all-features
+else
+    echo "Testing unoptimized implementation..."
+    cargo build --no-default-features --features valgrind,std,randomized-signer
+fi
+valgrind --tool=memcheck --error-exitcode=1 $SCRIPT_DIR/../target/debug/faest-memcheck 
+echo "Done."
